06070179f86293352eb0acded184570e499d1172
jdc-09 <juan.castillo.c@uniautonoma.edu.co>
2026-09-09 02:57:26 -0500

    Agregar validacion al formulario de reporte de casos

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function cambiarModo() {
     var elemento = document.body;
     elemento.classList.toggle("dark-mode");
 }
+
+function validarFormulario() {
+    let x = document.forms["formularioCaso"]["nombre"].value;
+    if (x.trim() === "") {
+        alert("El campo nombre debe ser llenado");
+        return false;
+    }
+}
